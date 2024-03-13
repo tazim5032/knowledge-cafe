aa1238a64038b3f3977d8930f0eb447085c0f9fd
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-03-13 16:15:01 +0600

    single blog component added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-6xl font-bold'>Knowledge Cafe</h1>
-        </div>
+        <header className='flex justify-between items-center p-4 mx-6 border-b-4'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
